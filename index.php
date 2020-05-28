f720e28f360f339cb27fc7b8c9a7bdf0446d88bb
Andrea De Martino <ademartino1992@gmail.com>
2020-05-28 14:17:45 +0200

    Create: Setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.css'/>
+  <link rel="stylesheet" href="dist/style/main.css">
+  <title>Document</title>
+</head>
+<body>
+  <h1>TEST1</h1>
+
+
+
+
+  <script src="dist/script/main.js"></script>
+</body>
+</html>
